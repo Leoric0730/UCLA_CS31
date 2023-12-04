d117776c5b4f5f8efe116e73c0c173177c182b77
Leoric Luo <128002801+Leoric0730@users.noreply.github.com>
2023-12-03 19:22:47 -0800

    Add files via upload

---
+++ midterm cs 31/midterm cs 31/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  midterm cs 31
+//
+//  Created by HaoYu Luo on 2022/5/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
